88123aa6557c0642b1be6c6305c8077de9618209
Tim Vu <timmyvu2020@gmail.com>
2020-08-14 23:09:55 -0400

    package.json

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const hbs = require("express-handlebars");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const bodyParser = require("body-parser");
 const { MongoClient } = require("mongodb");
 const uri =
